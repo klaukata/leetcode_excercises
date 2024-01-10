06ffba07012303b4bfe6aa40bcb4e21ab7e8362b
Klaudia <kkborowy@gmail.com>
2024-01-10 11:10:49 +0100

    2878 - my solution

--- python/intro_pandas.py
+++ python/intro_pandas.py
@@ -11,3 +11,7 @@ def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
     # d = {'student_id': student_id, 'age': age}
     # return pd.DataFrame(data=d)
     return pd.DataFrame(student_data, columns=['student_id', 'age'])
+
+#2878 - https://leetcode.com/problems/get-the-size-of-a-dataframe/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return [len(players), len(players.columns)]
