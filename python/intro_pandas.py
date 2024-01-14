bf78d93aa9a269f038d8eaa4c62ba19f58d9f20d
Klaudia <kkborowy@gmail.com>
2024-01-14 22:05:41 +0100

    2880 - my solution

--- python/intro_pandas.py
+++ python/intro_pandas.py
@@ -2,21 +2,35 @@
 
 import pandas as pd
 
-def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    # student_id = []
-    # age = []
-    # for x in student_data:
-    #     student_id.append(x[0])
-    #     age.append(x[1])
-    # d = {'student_id': student_id, 'age': age}
-    # return pd.DataFrame(data=d)
-    return pd.DataFrame(student_data, columns=['student_id', 'age'])
+# def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+#     # student_id = []
+#     # age = []
+#     # for x in student_data:
+#     #     student_id.append(x[0])
+#     #     age.append(x[1])
+#     # d = {'student_id': student_id, 'age': age}
+#     # return pd.DataFrame(data=d)
+#     return pd.DataFrame(student_data, columns=['student_id', 'age'])
 
 #2878 - https://leetcode.com/problems/get-the-size-of-a-dataframe/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
-def getDataframeSize(players: pd.DataFrame) -> List[int]:
-    # return [len(players), len(players.columns)]
-    return list(players.shape)
+# def getDataframeSize(players: pd.DataFrame) -> List[int]:
+#     # return [len(players), len(players.columns)]
+#     return list(players.shape)
 
 #2879 = https://leetcode.com/problems/display-the-first-three-rows/description/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
-def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
-    return employees.head(3)
+# def selectFirstRows(employees: pd.DataFrame) -> pd.DataFrame:
+#     return employees.head(3)
+
+
+data = {
+    "calories": [420, 380, 390],
+    "duration": [50, 40, 45],
+    "age": [20,21,22]
+}
+df = pd.DataFrame(data)
+
+#2880 - https://leetcode.com/problems/select-data/?envType=study-plan-v2&envId=introduction-to-pandas&lang=pythondata
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.query('calories == 420').loc[:, ['duration', 'age']]
+
+print(selectData(df))
